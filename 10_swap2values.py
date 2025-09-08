17897c72e2f1848060d89ef154d79fb1aee9cd47
Vanshika Khanna <vanshika23.k@gmail.com>
2025-09-09 00:38:54 +0530

    Update 10_swap2values.py

--- 10_swap2values.py
+++ 10_swap2values.py
@@ -2,7 +2,7 @@ a,b=5,10
 print("original a = ",a)
 print("original b=",b)
 print("\n")
-#Using a Temporary Variable
+
 print("Using a Temporary Variable")
 a,b = 5,10
 
@@ -12,7 +12,7 @@ b = temp
 
 print("a =", a, "b =", b)
 
-# Using Arithmetic: Addition & Subtraction
+
 print("Using Arithmetic: Addition & Subtraction")
 a,b = 5,10
 
@@ -22,7 +22,7 @@ a = a - b
 
 print("a =", a, "b =", b)
 
-#Using Multiplication & Division
+
 print("Using Multiplication & Division")
 a,b = 5,10
 
@@ -31,4 +31,5 @@ b = a / b
 a = a / b
 
 print("a =", int(a), "b =", int(b))
+
 
